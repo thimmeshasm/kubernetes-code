6081fa54e52be735ac9151e28ce574401af544bf
DevOps4Solutions <39961002+devops4solutions@users.noreply.github.com>
2025-02-13 22:40:27 -0500

    Update ebs.tf

--- terraform/eks_nodegroup/ebs.tf
+++ terraform/eks_nodegroup/ebs.tf
@@ -1,8 +1,3 @@
-provider "kubernetes" {
-   host                   = module.eks.cluster_endpoint
-   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-   token                  = data.aws_eks_cluster_auth.cluster.token
-}
 // This addon will also create a kubernetes service account with the necessary IAM role attached to managed EBS volumes
 resource "aws_eks_addon" "ebs" {
   depends_on = [ module.eks_managed_node_group ]
