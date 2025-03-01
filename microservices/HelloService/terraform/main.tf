46c42ab6d4d0f410a0150fe38a904eb41ce38dd8
Nidhi Gupta <nidhigupta@Nidhis-MacBook-Pro.local>
2025-03-01 17:41:27 -0500

    test

---
+++ microservices/HelloService/terraform/main.tf
@@ -0,0 +1,11 @@
+resource "helm_release" "myapp" {
+  name       = "hello-svc"
+  repository = "https://devops4solutions.github.io/springboot-helm-chart/" # Using the published Helm repo
+  chart      = "springboot"
+  namespace  = "app1"  # Change as per your setup
+  version    = "0.1.5"    # Use the correct version from index.yaml
+  create_namespace = "true"
+  values = [
+    file("${path.module}/values_hello.yaml")  # Optional: Use custom values file
+  ]
+}
---
+++ microservices/HelloService/terraform/providers.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = "us-east-1"
+  
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  }
+}
+
+
+
+terraform {
+  required_providers {
+    helm = {
+      source = "hashicorp/helm"
+          version = "2.17.0" 
+    }
+    kubectl = {
+      source = "bnu0/kubectl"
+      version = "0.27.0"
+    }
+  }
+
+  backend "s3" {
+    region = "us-east-1"
+    bucket = "devops4solutions-terraform"
+    key    = "eks/helloservice.tfstate"
+  }
+}
